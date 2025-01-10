af08890f02d55c187b92fc5b7abc12cf568bc27b
Evan Weeks <evan.weeks@parkplacetech.com>
2025-01-09 20:29:02 -0500

    Helps if you actually call the delegate.

--- src/xrm-mock/data/data.mock.ts
+++ src/xrm-mock/data/data.mock.ts
@@ -13,46 +13,46 @@ export class DataMock implements Xrm.Data {
     public getIsDirtyDelegate?: DataMockDataStateDelegate;
   
     constructor(entity: Xrm.Entity, process?: Xrm.ProcessFlow.ProcessManager) {
-      this.attributes = new ItemCollectionMock();
-      this.entity = entity;
-      this.process = process ? process : new ProcessManagerMock([]);
-      this.loadEventHandlers = [];
+        this.attributes = new ItemCollectionMock();
+        this.entity = entity;
+        this.process = process ? process : new ProcessManagerMock([]);
+        this.loadEventHandlers = [];
     }
   
     public refresh(save: boolean): XrmPromiseMock<undefined> {
-      return new XrmPromiseMock(new Promise(async (resolve) => resolve(this.refreshDelegate != null ? await this.refreshDelegate(save) : undefined)));
+        return this.refreshDelegate != null ? this.refreshDelegate(save) : new XrmPromiseMock(new Promise(async (resolve) => resolve(undefined)));
     }
   
     public save(saveOptions?: Xrm.SaveOptions): XrmPromiseMock<undefined> {
-      return new XrmPromiseMock(new Promise(async (resolve) => resolve(this.saveDelegate != null ? await this.saveDelegate(saveOptions) : undefined)));
+        return this.saveDelegate != null ? this.saveDelegate(saveOptions) : new XrmPromiseMock(new Promise(async (resolve) => resolve(undefined)));
     }
   
     public isValid(): boolean {
-      return this.isValidDelegate != null ? this.isValidDelegate() : true;
+        return this.isValidDelegate != null ? this.isValidDelegate() : true;
     }
   
     public getIsDirty(): boolean {
-      return this.getIsDirtyDelegate != null ? this.getIsDirtyDelegate() : false;
+        return this.getIsDirtyDelegate != null ? this.getIsDirtyDelegate() : false;
     }
   
     public addOnLoad(handler: Xrm.Events.DataLoadEventHandler): void {
-      this.loadEventHandlers.push(handler);
+        this.loadEventHandlers.push(handler);
     }
   
     public removeOnLoad(handler: Xrm.Events.ContextSensitiveHandler): void {
-      const index: number = this.loadEventHandlers.indexOf(handler);
-      this.loadEventHandlers.splice(index);
+        const index: number = this.loadEventHandlers.indexOf(handler);
+        this.loadEventHandlers.splice(index);
     }
   }
   
   export interface DataMockRefreshDelegate{
-    (save: boolean): XrmPromiseMock<undefined>;
+        (save: boolean): XrmPromiseMock<undefined>;
   }
   
   export interface DataMockSaveDelegate{
-    (saveOptions?: Xrm.SaveOptions): XrmPromiseMock<undefined>;
+        (saveOptions?: Xrm.SaveOptions): XrmPromiseMock<undefined>;
   }
   
   export interface DataMockDataStateDelegate{
-    (): boolean;
+        (): boolean;
   }
