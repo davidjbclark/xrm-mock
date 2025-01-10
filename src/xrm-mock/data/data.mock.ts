2851a6a405ded350d759846e15c45defee178cae
Evan Weeks <evan.weeks@parkplacetech.com>
2025-01-09 19:09:03 -0500

    Naming needed to be more specific on the interfaces.

--- src/xrm-mock/data/data.mock.ts
+++ src/xrm-mock/data/data.mock.ts
@@ -3,56 +3,56 @@ import { ProcessManagerMock } from '../processflow/processmanager/processmanager
 import { XrmPromiseMock } from '../async/xrmpromise/xrmpromise.mock';
 
 export class DataMock implements Xrm.Data {
-  public attributes: ItemCollectionMock<Xrm.Attributes.Attribute>;
-  public entity: Xrm.Entity;
-  public process: Xrm.ProcessFlow.ProcessManager;
-  public loadEventHandlers: Xrm.Events.DataLoadEventHandler[];
-  public refreshDelegate?: RefreshDelegate;
-  public saveDelegate?: SaveDelegate;
-  public isValidDelegate?: DataStateDelegate;
-  public getIsDirtyDelegate?: DataStateDelegate;
-
-  constructor(entity: Xrm.Entity, process?: Xrm.ProcessFlow.ProcessManager) {
-    this.attributes = new ItemCollectionMock();
-    this.entity = entity;
-    this.process = process ? process : new ProcessManagerMock([]);
-    this.loadEventHandlers = [];
+    public attributes: ItemCollectionMock<Xrm.Attributes.Attribute>;
+    public entity: Xrm.Entity;
+    public process: Xrm.ProcessFlow.ProcessManager;
+    public loadEventHandlers: Xrm.Events.DataLoadEventHandler[];
+    public refreshDelegate?: DataMockRefreshDelegate;
+    public saveDelegate?: DataMockSaveDelegate;
+    public isValidDelegate?: DataMockDataStateDelegate;
+    public getIsDirtyDelegate?: DataMockDataStateDelegate;
+  
+    constructor(entity: Xrm.Entity, process?: Xrm.ProcessFlow.ProcessManager) {
+      this.attributes = new ItemCollectionMock();
+      this.entity = entity;
+      this.process = process ? process : new ProcessManagerMock([]);
+      this.loadEventHandlers = [];
+    }
+  
+    public refresh(save: boolean): XrmPromiseMock<undefined> {
+      return new XrmPromiseMock(new Promise(async (resolve) => resolve(this.refreshDelegate != null ? await this.refreshDelegate(save) : undefined)));
+    }
+  
+    public save(saveOptions?: Xrm.SaveOptions): XrmPromiseMock<undefined> {
+      return new XrmPromiseMock(new Promise(async (resolve) => resolve(this.saveDelegate != null ? await this.saveDelegate(saveOptions) : undefined)));
+    }
+  
+    public isValid(): boolean {
+      return this.isValidDelegate != null ? this.isValidDelegate() : true;
+    }
+  
+    public getIsDirty(): boolean {
+      return this.getIsDirtyDelegate != null ? this.getIsDirtyDelegate() : false;
+    }
+  
+    public addOnLoad(handler: Xrm.Events.DataLoadEventHandler): void {
+      this.loadEventHandlers.push(handler);
+    }
+  
+    public removeOnLoad(handler: Xrm.Events.ContextSensitiveHandler): void {
+      const index: number = this.loadEventHandlers.indexOf(handler);
+      this.loadEventHandlers.splice(index);
+    }
   }
-
-  public refresh(save: boolean): XrmPromiseMock<undefined> {
-    return new XrmPromiseMock(new Promise(async (resolve) => resolve(this.refreshDelegate != null ? await this.refreshDelegate(save) : undefined)));
+  
+  export interface DataMockRefreshDelegate{
+    (save: boolean): XrmPromiseMock<undefined>;
   }
-
-  public save(saveOptions?: Xrm.SaveOptions): XrmPromiseMock<undefined> {
-    return new XrmPromiseMock(new Promise(async (resolve) => resolve(this.saveDelegate != null ? await this.saveDelegate(saveOptions) : undefined)));
+  
+  export interface DataMockSaveDelegate{
+    (saveOptions?: Xrm.SaveOptions): XrmPromiseMock<undefined>;
   }
-
-  public isValid(): boolean {
-    return this.isValidDelegate != null ? this.isValidDelegate() : true;
+  
+  export interface DataMockDataStateDelegate{
+    (): boolean;
   }
-
-  public getIsDirty(): boolean {
-    return this.getIsDirtyDelegate != null ? this.getIsDirtyDelegate() : false;
-  }
-
-  public addOnLoad(handler: Xrm.Events.DataLoadEventHandler): void {
-    this.loadEventHandlers.push(handler);
-  }
-
-  public removeOnLoad(handler: Xrm.Events.ContextSensitiveHandler): void {
-    const index: number = this.loadEventHandlers.indexOf(handler);
-    this.loadEventHandlers.splice(index);
-  }
-}
-
-interface RefreshDelegate{
-  (save: boolean): XrmPromiseMock<undefined>;
-}
-
-interface SaveDelegate{
-  (saveOptions?: Xrm.SaveOptions): XrmPromiseMock<undefined>;
-}
-
-interface DataStateDelegate{
-  (): boolean;
-}
